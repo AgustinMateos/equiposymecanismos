9b1dfd6ed5f5155a05511ed11a478ab9b80d676a
delfinblaksleymujica <delf.blaksley@hotmail.com>
2025-08-01 20:40:11 -0300

    Agregado de analytics

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 import Navbar from "@/components/generals/Navbar";
 import Footer from "@/components/generals/Footer";
@@ -18,11 +19,33 @@ export const metadata = {
   title: "EQUIMEC | Equipos y mecanismos para la purificación de agua",
   description:
     "Soluciones eficaces para la purificación y ablandamiento del agua. Protegé tus cañerías y electrodomésticos con nuestros sistemas de tratamiento.",
+  // other: {
+  //   "google-site-verification": "TU-CODIGO-DE-VERIFICACION",
+  // },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* Esta parte se incluirá en el head real si no usás app/head.jsx */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-MBJKK8JCF7"
+          strategy="afterInteractive"
+        />
+        <Script
+          id="gtag-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-MBJKK8JCF7');
+            `,
+          }}
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
